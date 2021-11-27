568265377de5d734ee6d80de3c4a0b3b211c971e
semreguleryuz24 <s.emreguleryuz@gmail.com>
2021-11-28 01:48:15 +0300

    Add project files.

---
+++ SecondCoreMvc/Controllers/HomeController.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SecondCoreMvc.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SecondCoreMvc.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public string Index()
+        {
+            return "HELLO!! First From Project";
+        }
+        public ViewResult Index2()
+        {
+            List<Employee> employees = new List<Employee>
+            {
+                new Employee{Id=1,FirstName="Sırrı Emre",LastName="GÜLERYÜZ",CityId=6},
+                new Employee{Id=2,FirstName="Emir Taha",LastName="GÜLERYÜZ",CityId=6},
+                new Employee{Id=3,FirstName="Çınar",LastName="AYAZ",CityId=5},
+                new Employee{Id=4,FirstName="Defne",LastName="AYAZ",CityId=5}
+            };
+            List<string> cities = new List<string> { "İstanbul", "Ankara" };
+
+            var model = new EmployeeListViewModel
+            {
+                Employee = employees,
+                Cities = cities
+            };
+
+
+            return View(model);
+        }
+        public StatusCodeResult Index4()
+        {
+            return Ok();
+        }
+        public StatusCodeResult Index5()
+        {
+            return NotFound();
+        }
+        public RedirectResult Index6()
+        {
+            return Redirect("/Home/Index2");
+        }
+        public IActionResult Index7()
+        {
+            return RedirectToRoute("default");
+        }
+        public JsonResult Index8()
+        {
+            List<Employee> employees = new List<Employee>
+            {
+                new Employee{Id=1,FirstName="Sırrı Emre",LastName="GÜLERYÜZ",CityId=6},
+                new Employee{Id=2,FirstName="Emir Taha",LastName="GÜLERYÜZ",CityId=6},
+                new Employee{Id=3,FirstName="Çınar",LastName="AYAZ",CityId=5},
+                new Employee{Id=4,FirstName="Defne",LastName="AYAZ",CityId=5}
+            };
+            return Json(employees);
+        }
+        public IActionResult RazorDemo()
+        {
+            List<Employee> employees = new List<Employee>
+            {
+                new Employee{Id=1,FirstName="Sırrı Emre",LastName="GÜLERYÜZ",CityId=6},
+                new Employee{Id=2,FirstName="Emir Taha",LastName="GÜLERYÜZ",CityId=6},
+                new Employee{Id=3,FirstName="Çınar",LastName="AYAZ",CityId=5},
+                new Employee{Id=4,FirstName="Defne",LastName="AYAZ",CityId=5}
+            };
+            List<string> cities = new List<string> { "İstanbul", "Ankara" };
+
+            var model = new EmployeeListViewModel
+            {
+                Employee = employees,
+                Cities = cities
+            };
+
+
+            return View(model);
+
+        }
+    }
+}
+//http://engindemirog.com/home/index
+//HomeController h=new Controller();
+//h.Index();
+
---
+++ SecondCoreMvc/Models/EmployeeListViewModel.cs
@@ -0,0 +1,11 @@
+﻿using SecondCoreMvc.Entities;
+using System.Collections.Generic;
+
+namespace SecondCoreMvc
+{
+    public class EmployeeListViewModel
+    {
+        public List<Employee> Employee { get; set; }
+        public List<string> Cities { get; set; }
+    }
+}
